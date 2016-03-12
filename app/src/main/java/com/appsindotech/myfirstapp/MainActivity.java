822e7f1ef524192e0c9835240e54d92352a1b35a
Andrew Kurniadi <andrew.kurniadi@at.co.id>
2016-03-12 16:24:30 +0700

    Initial Commit

---
+++ app/src/main/java/com/appsindotech/myfirstapp/MainActivity.java
@@ -0,0 +1,90 @@
+package com.appsindotech.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.button)
+    Button button1;
+    @Bind(R.id.button2)
+    Button button2;
+    @Bind(R.id.button3)
+    Button button3;
+    @Bind(R.id.button4)
+    Button button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the second Activity and pass the note_id
+                Intent showDetailViewIntent = new Intent(MainActivity.this, DetailActivity.class);
+                showDetailViewIntent.putExtra("note_id", 2);
+                startActivity(showDetailViewIntent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Button 3 clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        button4.setText("show Congratulations Message");
+    }
+
+    @OnClick({R.id.button3, R.id.button4})
+    public void showToastCongratulations(Button btn)
+    {
+        Toast.makeText(MainActivity.this, "Congrats " + btn.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
